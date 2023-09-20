352e2aa9a594c0794e440cd37eeacfdd787483d8
crossrepublic <71635944+ziminl@users.noreply.github.com>
2023-09-20 22:41:50 +0900

    Update post_r_test.py

--- post_r_test.py
+++ post_r_test.py
@@ -4,7 +4,12 @@
 import json
 import requests
 
+
+
 url = "http://127.0.0.1:5000/wins/1"#http://<host>/wins/<username>
+
+
+
 headers = {'Content-type': 'application/json'}
 data = {'username': '1'}
 r = requests.post(url, data=json.dumps(data), headers=headers)
